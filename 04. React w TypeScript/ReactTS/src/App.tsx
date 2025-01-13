98b28f80faac1caf2edafeebc8636d58f8dcb42f
Shahid Khan <shahidseran786@gmail.com>
2025-01-13 15:56:23 +0530

    React TS Init

---
+++ 04. React w TypeScript/ReactTS/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ComponentsProtoTyping from './Components/01Component_Props/ComponentsProtoTyping'
+
+const App = () => {
+  return (
+    <div>
+      <ComponentsProtoTyping/>
+    </div>
+  )
+}
+
+export default App
